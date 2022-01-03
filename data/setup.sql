24e96ae2ae334f556a3b9a09d72e663834cab746
takabayap <takabayap@gmail.com>
2022-01-03 11:43:43 +0900

    update: add chatroom table to database

--- data/setup.sql
+++ data/setup.sql
@@ -1,4 +1,11 @@
-create table posts(
+
+create table chatrooms (
+    id      serial primary key,
+    uuid    varchar(64) not null unique,
+    name    text 
+);
+
+create table posts (
 	id		serial primary key,
 	body		text,
 	user_id	 	integer,
