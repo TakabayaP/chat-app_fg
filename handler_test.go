4db7d53675700573f8652701cbe5d8b3799da23c
takabayap <takabayap@gmail.com>
2022-04-10 17:00:03 +0900

    update: add TestChatroomHandler

--- handler_test.go
+++ handler_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -20,4 +22,30 @@ func TestIndexHandler(t *testing.T) {
 	if string(data) != "chat server running!" {
 		t.Errorf("expected chat server running! got %v", string(data))
 	}
+}
+
+type mockChatroomDb struct{}
+
+func (m *mockChatroomDb) Chatrooms() []Chatroom {
+	return []Chatroom{{Id: 1, Name: "test"}, {Id: 2, Name: "test2"}}
+}
+func TestChatroomHandler(t *testing.T) {
+	Db2 = &mockChatroomDb{}
+	result := getterHandlerTester(handleChatroomSelect, "/chatrooms")
+	body, _ := ioutil.ReadAll(result.Body)
+	fmt.Println(string(body))
+	var chatrooms []Chatroom
+	json.Unmarshal([]byte(body), &chatrooms)
+	fmt.Println(chatrooms)
+	if chatrooms[1].Id == 2 && chatrooms[1].Name != "test2" {
+		t.Errorf("chatroom Id:2 should have the name test2 but instead had %s", chatrooms[1].Name)
+	}
+}
+
+func getterHandlerTester(handler func(http.ResponseWriter, *http.Request), address string) http.Response {
+	req := httptest.NewRequest(http.MethodGet, address, nil)
+	rec := httptest.NewRecorder()
+	handler(rec, req)
+
+	return *rec.Result()
 }
