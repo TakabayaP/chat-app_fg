ff1a456746b04acfc913188fc4135fac1d6ff65b
takabayap <takabayap@gmail.com>
2022-04-14 17:27:49 +0900

    update: add testchatroomcreatehandler

--- chatroom_test.go
+++ chatroom_test.go
@@ -1,6 +1,7 @@
-package main
+package chat
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -14,9 +15,9 @@ type mockChatroomDb struct{}
 func (m *mockChatroomDb) Chatrooms() []Chatroom {
 	return []Chatroom{{Id: 1, Name: "test"}, {Id: 2, Name: "test2"}}
 }
-func TestChatroomSelectHandler(t *testing.T) {
+func TestChatroomGetHandler(t *testing.T) {
 	Db2 = &mockChatroomDb{}
-	result := getterHandlerTester(handleChatroomSelect, "/chatrooms")
+	result := getHandlerTester(handleChatroomGet, "/chatrooms")
 	body, _ := ioutil.ReadAll(result.Body)
 	fmt.Println(string(body))
 	var chatrooms []Chatroom
@@ -34,13 +35,23 @@ func getHandlerTester(handler func(http.ResponseWriter, *http.Request), address 
 
 	return *rec.Result()
 }
-func postHandlerTester(handler func(http.ResponseWriter, *http.Request), address string) http.Response {
 
-	req := httptest.NewRequest(http.MethodGet, address, nil)
+func postHandlerTester(handler func(http.ResponseWriter, *http.Request), address string, data string) http.Response {
+	req := httptest.NewRequest(http.MethodPost, address, bytes.NewBufferString(data))
 	rec := httptest.NewRecorder()
+	req.Header.Set("Content-Type", "application/json; charset=UTF-8")
 	handler(rec, req)
 	return *rec.Result()
 }
 func TestChatroomCreateHandler(t *testing.T) {
 	Db2 := &mockChatroomDb{}
+	result := postHandlerTester(handleChatroomCreate, "/chatrooms", `{"name":"test3","id":"3"}`)
+	body, _ := ioutil.ReadAll(result.Body)
+	fmt.Println(string(body))
+	var chatrooms []Chatroom
+	json.Unmarshal([]byte(body), &chatrooms)
+	fmt.Println(chatrooms)
+	if chatrooms[2].Id == 3 && chatrooms[2].Name != "test3" {
+		t.Errorf("chatroom Id:3 should have the name test3 but instead had %s", chatrooms[1].Name)
+	}
 }
