50fa5b0bd16c49c08b95d8fe64c612317dfded63
takabayap <takabayap@gmail.com>
2022-04-18 17:36:27 +0900

    update: add create method to mockChatroomDb

--- chatroom_test.go
+++ chatroom_test.go
@@ -12,10 +12,17 @@ import (
 
 type mockChatroomDb struct{}
 
+var test_chatroom_list []Chatroom
+
 func (m *mockChatroomDb) Chatrooms() []Chatroom {
-	return []Chatroom{{Id: 1, Name: "test"}, {Id: 2, Name: "test2"}}
+	return test_chatroom_list
+}
+func (m *mockChatroomDb) CreateRoom(chatroom Chatroom) []Chatroom {
+	test_chatroom_list = append(test_chatroom_list, chatroom)
+	return m.Chatrooms()
 }
 func TestChatroomGetHandler(t *testing.T) {
+	test_chatroom_list = []Chatroom{{Id: 1, Name: "test"}, {Id: 2, Name: "test2"}}
 	Db2 = &mockChatroomDb{}
 	result := getHandlerTester(handleChatroomGet, "/chatrooms")
 	body, _ := ioutil.ReadAll(result.Body)
@@ -25,6 +32,20 @@ func TestChatroomGetHandler(t *testing.T) {
 	fmt.Println(chatrooms)
 	if chatrooms[1].Id == 2 && chatrooms[1].Name != "test2" {
 		t.Errorf("chatroom Id:2 should have the name test2 but instead had %s", chatrooms[1].Name)
+	}
+}
+
+func TestChatroomCreateHandler(t *testing.T) {
+	test_chatroom_list = []Chatroom{{Id: 1, Name: "test"}, {Id: 2, Name: "test2"}}
+	Db2 = &mockChatroomDb{}
+	result := postHandlerTester(handleChatroomCreate, "/chatrooms", `{"name":"test3","id":"3"}`)
+	body, _ := ioutil.ReadAll(result.Body)
+	fmt.Println(string(body))
+	var chatrooms []Chatroom
+	json.Unmarshal([]byte(body), &chatrooms)
+	fmt.Println(chatrooms)
+	if chatrooms[2].Id == 3 && chatrooms[2].Name != "test3" {
+		t.Errorf("chatroom Id:3 should have the name test3 but instead had %s", chatrooms[1].Name)
 	}
 }
 
@@ -42,16 +63,4 @@ func postHandlerTester(handler func(http.ResponseWriter, *http.Request), address
 	req.Header.Set("Content-Type", "application/json; charset=UTF-8")
 	handler(rec, req)
 	return *rec.Result()
-}
-func TestChatroomCreateHandler(t *testing.T) {
-	Db2 := &mockChatroomDb{}
-	result := postHandlerTester(handleChatroomCreate, "/chatrooms", `{"name":"test3","id":"3"}`)
-	body, _ := ioutil.ReadAll(result.Body)
-	fmt.Println(string(body))
-	var chatrooms []Chatroom
-	json.Unmarshal([]byte(body), &chatrooms)
-	fmt.Println(chatrooms)
-	if chatrooms[2].Id == 3 && chatrooms[2].Name != "test3" {
-		t.Errorf("chatroom Id:3 should have the name test3 but instead had %s", chatrooms[1].Name)
-	}
 }
